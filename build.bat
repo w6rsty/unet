05b53e43f1f186d8ceb92de7df086d54c9083faa
w6rsty <1486008923@qq.com>
2024-05-26 12:55:42 +0800

    更新构建脚本

--- build.bat
+++ build.bat
@@ -11,8 +11,3 @@ pyinstaller --onefile app.py --noconsole --icon=assets/unet.ico --name=unet
 copy dist\unet.exe output\unet.exe
 
 xcopy assets output\assets /E /Y
-
-echo D
-
-rmdir /s /q build
-rmdir /s /q dist
